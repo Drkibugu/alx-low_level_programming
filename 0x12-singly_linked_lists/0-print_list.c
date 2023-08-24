5dc281b140ae0c32a8dd7eb4e6025cfec296fff8
Drkibugu <fionakibugu4@gmail.com>
2023-08-24 12:15:53 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
-*Print_list - This  prints all the elements of a linked list
-*@h: pointer to the list_t list to print
-*
-*Return: the number of nodes printed
+*print_list - This  prints all the elements of a linked list
+*@h: This pointer to the list_t list to print
+*Drkibugu
+*Return: This is  the number of nodes printed
 */
 size_t print_list(const list_t *h)
 {
