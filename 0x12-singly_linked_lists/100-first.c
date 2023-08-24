613efd82513f2c39b34fce72a7f748ad319c0ef5
Drkibugu <fionakibugu4@gmail.com>
2023-08-24 12:28:18 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,8 @@
 void first(void) __attribute__ ((constructor));
 
 /**
-* first - prints a sentence before the main
-* function is executed
+* first - This prints a sentence before the main
+* function is executed by Drkibugu
 */
 void first(void)
 {
