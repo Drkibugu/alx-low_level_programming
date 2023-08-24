88705e497da1c18eff8be3336a827514d0ca0607
Drkibugu <fionakibugu4@gmail.com>
2023-08-24 12:23:12 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,10 +3,10 @@
 #include "lists.h"
 
 /**
-* add_node_end - adds a new node at the end of a linked list
-* @head: double pointer to the list_t list
-* @str: string to put in the new node
-*
+* add_node_end - This adds a new node at the end of a linked list
+* @head: This is a  double pointer to the list_t list
+* @str: This is a string to put in the new node
+* Drkibugu
 * Return: address of the new element, or NULL if it failed
 */
 list_t *add_node_end(list_t **head, const char *str)
