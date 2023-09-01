9f91e59ddff8446baa77e5a287abef3a09cccbfc
Drkibugu <fionakibugu4@gmail.com>
2023-09-01 08:58:07 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - This fn prints the binary equivalent of a decimal number
+ * @n: Represents a number to print in binary in Drkibugus code
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
